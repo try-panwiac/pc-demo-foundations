fa8b3fe6e488f63f18404eea4fcb13f7248c6b53
alexandre-cezar <alexandre_cezar@yahoo.com.br>
2023-02-16 15:32:39 -0500

    change the log4j script

--- scripts/port_scan.sh
+++ scripts/port_scan.sh
@@ -4,8 +4,8 @@ private_subnet="172.20.2.0/24"
 
 # Executes nmap SYN scan against the private subnet
 echo "Starting SYN scan..."
-nmap -sS $private_subnet
+sudo nmap -sS $private_subnet
 
 # Execute nmap UDP scan against the private subnet
 echo "Starting UDP scan..."
-nmap -sU $private_subnet
+sudo nmap -sU $private_subnet
